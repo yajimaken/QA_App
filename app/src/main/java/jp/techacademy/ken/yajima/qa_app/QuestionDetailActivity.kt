da234baa06816cde1d2fd66751792297d398cc21
Ken Yajima <yaji_ken@icloud.com>
2019-07-07 16:37:06 +0900

    AnswerSendActivity

--- app/src/main/java/jp/techacademy/ken/yajima/qa_app/QuestionDetailActivity.kt
+++ app/src/main/java/jp/techacademy/ken/yajima/qa_app/QuestionDetailActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
 import android.support.v7.app.AppCompatActivity
+import android.util.Base64
 import android.view.View
 import android.widget.ListView
 
@@ -19,12 +20,81 @@ import kotlinx.android.synthetic.main.list_question_detail.*
 
 import java.util.HashMap
 
-class QuestionDetailActivity : AppCompatActivity(),View.OnClickListener{
+class QuestionDetailActivity : AppCompatActivity(){
 
     private lateinit var mQuestion: Question
     private lateinit var mAdapter: QuestionDetailAdapter
     private lateinit var mAnswerRef: DatabaseReference
+    private lateinit var mQuestionArrayList: ArrayList<Question>
     private lateinit var mAuth: FirebaseAuth
+
+    //**************************************************************************************************************************************************:
+    private val mfEventListener = object : ChildEventListener {
+        override fun onChildAdded(dataSnapshot: DataSnapshot, s: String?) {
+            val map = dataSnapshot.value as Map<String, String>
+            val title = map["title"] ?: ""
+            val body = map["body"] ?: ""
+            val name = map["name"] ?: ""
+            val uid = map["uid"] ?: ""
+            val imageString = map["image"] ?: ""
+            val bytes =
+                if (imageString.isNotEmpty()) {
+                    Base64.decode(imageString, Base64.DEFAULT)
+                } else {
+                    byteArrayOf()
+                }
+
+            val answerArrayList = ArrayList<Answer>()
+            val answerMap = map["answers"] as Map<String, String>?
+            if (answerMap != null) {
+                for (key in answerMap.keys) {
+                    val temp = answerMap[key] as Map<String, String>
+                    val answerBody = temp["body"] ?: ""
+                    val answerName = temp["name"] ?: ""
+                    val answerUid = temp["uid"] ?: ""
+                    val answer = Answer(answerBody, answerName, answerUid, key)
+                    answerArrayList.add(answer)
+                }
+            }
+
+            val question =
+                Question(title, body, name, uid, dataSnapshot.key ?: "", mQuestion.genre, bytes, answerArrayList)
+            mQuestionArrayList.add(question)
+            mAdapter.notifyDataSetChanged()
+        }
+
+        override fun onChildChanged(dataSnapshot: DataSnapshot, s: String?) {
+            val map = dataSnapshot.value as Map<String, String>
+
+            for (question in mQuestionArrayList) {
+                if (dataSnapshot.key.equals(question.questionUid)) {
+                    question.answers.clear()
+                    val answerMap = map["answers"] as Map<String, String>?
+                    if (answerMap != null) {
+                        for (key in answerMap.keys) {
+                            val temp = answerMap[key] as Map<String, String>
+                            val answerBody = temp["body"] ?: ""
+                            val answerName = temp["name"] ?: ""
+                            val answerUid = temp["uid"] ?: ""
+                            val answer = Answer(answerBody, answerName, answerUid, key)
+                            question.answers.add(answer)
+                        }
+                    }
+
+                    mAdapter.notifyDataSetChanged()
+                }
+            }
+        }
+        override fun onChildRemoved(p0: DataSnapshot) {
+        }
+        override fun onChildMoved(p0: DataSnapshot, p1: String?) {
+        }
+        override fun onCancelled(p0: DatabaseError) {
+
+        }
+    }
+    //**************************************************************************************************************************************************:
+
 
     private val mEventListener = object : ChildEventListener {
         override fun onChildAdded(dataSnapshot: DataSnapshot, s: String?) {
@@ -45,6 +115,7 @@ class QuestionDetailActivity : AppCompatActivity(),View.OnClickListener{
             val answer = Answer(body, name, uid, answerUid)
             mQuestion.answers.add(answer)
             mAdapter.notifyDataSetChanged()
+
         }
 
         override fun onChildChanged(dataSnapshot: DataSnapshot, s: String?) {
@@ -68,7 +139,6 @@ class QuestionDetailActivity : AppCompatActivity(),View.OnClickListener{
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_question_detail)
 
-
         val extras = intent.extras
         mQuestion = extras.get("question") as Question
 
@@ -89,40 +159,28 @@ class QuestionDetailActivity : AppCompatActivity(),View.OnClickListener{
                 intent.putExtra("question", mQuestion)
                 startActivity(intent)
             }
-
         }
+
 
 
         val dataBaseReference = FirebaseDatabase.getInstance().reference
         mAnswerRef = dataBaseReference.child(ContentsPATH).child(mQuestion.genre.toString()).child(mQuestion.questionUid).child(AnswersPATH)
         mAnswerRef.addChildEventListener(mEventListener)
 
+
         favorite_button.setOnClickListener {
-            val databaseReference=FirebaseDatabase.getInstance().reference
-            val user=mAuth.currentUser
-            mAnswerRef = databaseReference.child(UsersPATH).child(user!!.uid).child(FavoriteQ).child(mQuestion.questionUid).child(mQuestion.genre.toString())
-            val extras = intent.extras
-            mQuestion = extras.get("question") as Question
+            val databaseReference = FirebaseDatabase.getInstance().reference
+            val user = FirebaseAuth.getInstance().currentUser
+            if (user == null) {
 
-            val favorite=mQuestion.questionUid
-            val data=HashMap<String,String>()
-            data["favorite"]=favorite
-            mAnswerRef.push().setValue(data,this)
-        }
-    }
+            } else {
+                val favoriteRef = databaseReference.child(UsersPATH).child(user!!.uid).child(FavoriteQ).child(mQuestion.questionUid)
+                val data = HashMap<String, String>()
+                data["genre"] = mQuestion.genre.toString()
+                favoriteRef.setValue(data)
 
-    override fun onClick(v: View) {
-        if(v==favorite_button){
-            val databaseReference=FirebaseDatabase.getInstance().reference
-            val user=mAuth.currentUser
-            mAnswerRef = databaseReference.child(UsersPATH).child(user!!.uid).child(FavoriteQ)
-            val extras = intent.extras
-            mQuestion = extras.get("question") as Question
-
-            val favorite=mQuestion.questionUid
-            val data=HashMap<String,String>()
-            data["favorite"]=favorite
-            mAnswerRef.push().setValue(data,this)
+                favoriteRef!!.addChildEventListener(mfEventListener)
+            }
         }
     }
 }
